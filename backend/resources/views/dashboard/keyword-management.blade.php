749fb751acc6b8cdbb3b60b3f6bbfc78268ac886
vishalshah84 <95851036+vishalshah84@users.noreply.github.com>
2023-04-06 14:07:46 +0530

    Update keyword-management.blade.php

--- backend/resources/views/dashboard/keyword-management.blade.php
+++ backend/resources/views/dashboard/keyword-management.blade.php
@@ -81,7 +81,7 @@
                 <div class="col-sm-10">
                     <select style="height:300px; !important" data-placeholder="Context Tags" id="context_tags" name="context_tags[]" multiple class="chosen-select form-control">
 
-                        <option value=""></option>
+                        <option value="null"></option>
 
                         @foreach ($all_context_names as $key => $data)
 
