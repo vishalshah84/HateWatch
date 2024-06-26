1163fc4062a483728d8f96c242da44142e4fca8d
Vishal Shah <95851036+vishalshah84@users.noreply.github.com>
2024-06-26 07:26:49 +0530

    Update the-project-HateWatch.sql

--- database/the-project-HateWatch.sql
+++ database/the-project-HateWatch.sql
@@ -62,8 +62,8 @@ CREATE TABLE `knowledgebase` (
 --
 
 INSERT INTO `knowledgebase` (`auto_id`, `keyword`, `context_tags`, `description`, `language`) VALUES
-(15, 'ලොක්කො', '[\"Gender\",\"LGBTQ\"]', '[NULL]', 'hindi'),
-(16, 'ගෙදර', '[\"Gender\",\"religion\"]', '[NULL]', 'hindi'),
+(15, 'ලො', '[\"Gender\",\"LGBTQ\"]', '[NULL]', 'hindi'),
+(16, 'ගර', '[\"Gender\",\"religion\"]', '[NULL]', 'hindi'),
 (17, 'ගියෙ', '[\"LGBT\"]', '[NULL]', 'hindi');
 
 -- --------------------------------------------------------
