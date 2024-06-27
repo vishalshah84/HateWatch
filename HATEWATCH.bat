2ae560a6a96402bdfca24345086c4ff441535ca1
Vishal Shah <95851036+vishalshah84@users.noreply.github.com>
2024-06-28 00:54:06 +0530

    Update HATEWATCH.bat

--- HATEWATCH.bat
+++ HATEWATCH.bat
@@ -1,5 +1,5 @@
-start C:\xampp\xampp-control.exe
+start C:\xampp1\xampp-control.exe
 
-cd C:\xampp\htdocs\Hatewatch
+cd C:\xampp1\htdocs\Hatewatch
 
 cd cyber & start /B python app.py & cd .. & cd backend & start chrome http://127.0.0.1:8000 & php artisan serve
